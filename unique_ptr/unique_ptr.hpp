8c8fdfeffb6def99df0c3fa717b48a250bb6991f
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2026-08-16 14:04:21 -0500

    Class of unique_ptr

---
+++ unique_ptr/unique_ptr.hpp
@@ -0,0 +1,16 @@
+#pragma
+
+namespace tynystl {
+template<class T>
+class unique_ptr {
+public:
+    unique_ptr() : p_(nullptr) {}
+    explicit unique_ptr(T* p) : p_(p) {}
+    ~unique_ptr() {delete p_; }
+    T* get() const { return p_; }
+    
+private:
+    T* p_;
+};
+
+}
